eba1fae135ee548a1a9e512552ae70d88753fa36
neele <neelesh6kumar@gmail.com>
2025-10-13 01:56:23 +0530

    day 2 question

--- src/dsa_basic/reverse_num.java
+++ src/dsa_basic/reverse_num.java
@@ -1,8 +1,35 @@
 package dsa_basic;
 import java.util.*; //import java utilities
 
+
 public class reverse_num {
 
+public static void main(String[] args){
+    Scanner scn = new Scanner(System.in);
+    int n =scn.nextInt();
 
+    int nod =0;
+    int temp =n;
+    while(temp>0){
+        temp= temp/10;
+        nod++;
+    }
+    System.out.print(nod);
+
+    int div =(int)Math.pow(10 , nod-1);
+int p=0;
+
+    while(n>0){
+        int q = n % 10;
+        n = n/10;
+
+
+        p += q*div;
+        div=div/10;
+
+    }
+    System.out.print(p);
+
+}
 
 }
