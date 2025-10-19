5cc99b6169f7b1b48c4f502dde5c2df6e8dcc2d7
neele <neelesh6kumar@gmail.com>
2025-10-19 16:33:11 +0530

    day5

---
+++ src/pattern/pattern4.java
@@ -0,0 +1,30 @@
+package pattern;
+import java.util.*;
+
+public class pattern4 {
+    public static void main(String[] argd){
+        Scanner scn = new Scanner(System.in);
+
+        int n = scn.nextInt();
+
+        int space =0;
+        int star =n;
+        for(int i=1; i<=n; i++){
+
+            for(int j=1; j<=star; j++){
+                System.out.print("*");
+            }
+
+            for(int j=1; j<=space; j++){
+                System.out.print("-");
+
+            }
+
+            System.out.println();
+            star--;
+            space++;
+
+        }
+    }
+
+}
