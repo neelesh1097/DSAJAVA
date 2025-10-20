bab3146b699b325c2f4d82da5baadb9b9487be6c
neele <neelesh6kumar@gmail.com>
2025-10-20 22:08:42 +0530

    day6

---
+++ src/pattern/pattern14.java
@@ -0,0 +1,16 @@
+package pattern;
+import java.util.Scanner;
+
+public class pattern14 {
+
+    public static void main(String[] args){
+
+        Scanner scn = new Scanner(System.in);
+
+        int n = scn.nextInt();
+
+
+
+
+    }
+}
