5f19a232471f966e4c0184b85df47f116569b4d3
neele <neelesh6kumar@gmail.com>
2025-10-27 23:43:13 +0530

    day10

---
+++ src/introduction_functions/add_base.java
@@ -0,0 +1,14 @@
+package introduction_functions;
+import java.util.*;
+
+
+public class add_base {
+
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int j =scn.nextInt();
+
+
+}
+
+public static int Get
